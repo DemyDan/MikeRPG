c9343c92af38d1c4ac9f4c2890fe0a8c8e2c7a84
Demy <32939076+DemyDan@users.noreply.github.com>
2018-11-14 00:42:50 +0100

    Yeet

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    public Transform target;
+
+    public float distance = 10.0f;
+    public float sensitivity = 3.0f;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        //Pakt de afstand tussen de camera positie en target positie. Zorgt ervoor dat deze in stukjes gehakt word en dat kan dat 
+        //keer het aantal keer gedaan word wat jij wilt
+        offset = (transform.position - target.position).normalized * distance;
+
+        //Initalizes Position to target position + the offset
+        transform.position = target.position + offset;
+    }
+
+    void Update()
+    {
+        //Maak een quaternion aan die een rotation is om een object, pak deze via horizontale muisbewegingen * sensitivty
+        Quaternion turnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * sensitivity, Vector3.up);
+
+        //Pas deze quaternion toe op de offset
+        offset = turnAngle * offset;
+
+        //Update de rotatie van de camera door het keer de muisinput quaternion te doen
+        transform.rotation = turnAngle * transform.rotation;
+
+        //Updates the camera position to target position + the offset
+        transform.position = target.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    CharacterController cc;
+
+    public Transform cam;
+
+    public float gravity;
+    public float speed;
+    public float playerStill;
+
+    Vector3 movement;
+
+    void Start()
+    {
+        Cursor.visible = false;
+
+        playerStill = this.transform.rotation.y;
+
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        //Pakt de 2 vectoren die nodig zijn voor horizontaal en verticaal lopen en stopt ze in een vector 2
+        Vector2 inputDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        //Zorgt ervoor dat de movement niet laggy is, hoeft ie nog niet helemaal te weten
+        if (inputDirection.sqrMagnitude > 1)
+        {
+            inputDirection = inputDirection.normalized;
+        }
+
+        /**
+         * 
+         *      public static Vector3 Cross(Vector3 lhs, Vector3 rhs)
+        {
+            return new Vector3(
+                lhs.y * rhs.z - lhs.z * rhs.y,
+                lhs.z * rhs.x - lhs.x * rhs.z,
+                lhs.x * rhs.y - lhs.y * rhs.x);
+        }
+        **/
+        //Pakt de rechter kant van de vector
+        Vector3 newRight = Vector3.Cross(Vector3.up, cam.forward);
+
+        //Pakt de voorkant van de vector
+        Vector3 newForward = Vector3.Cross(newRight, Vector3.up);
+
+        //Pak de uitkomensten van de vectoren en doe ze keer je input, plus teken is voor als je beide kanten op wilt, doe dan de vectoren plus elkaar
+        Vector3 movement = (newRight * inputDirection.x) + (newForward * inputDirection.y);
+
+        //Zorgt ervoor dat je poppetje niet zweeft
+        movement.y -= (gravity * Time.deltaTime);
+
+        //Move je CC keer de speed die jij wilt
+        cc.Move(movement * speed);
+
+        //Pak de rotatie van de player en maak die op de y as hetzelfde als die van de camera
+        transform.rotation = Quaternion.Euler(0, cam.transform.rotation.eulerAngles.y, 0);
+    }
+}
