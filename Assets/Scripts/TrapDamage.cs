ef667036281a177c3c0935ead632f8b6bd191d6d
DemyDan <demydanielsson@gmail.com>
2018-11-18 15:32:55 +0100

    Yea

---
+++ Assets/Scripts/TrapDamage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapDamage : MonoBehaviour {
+
+    public int damage;
+
+    PlayerController hero;
+
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            hero = player.GetComponent<PlayerController>();
+
+            hero.Damage(damage);
+        }
+    }
+}
