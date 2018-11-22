ac37bcdc778e8577668e2b553a69d8f8255623f1
DemyDan <demydanielsson@gmail.com>
2018-11-22 18:08:50 +0100

    Begin scene

---
+++ Assets/MenuButtons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadScene(string scene)
+    {
+        Application.LoadLevel(scene);
+    }
+
+    public void ExitApp()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
